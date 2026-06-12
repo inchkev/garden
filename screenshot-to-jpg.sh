61f66c76a20f51431ace9ce087f90f4e753699b5
Kevin Chen <kchen1250@gmail.com>
2026-06-11 22:42:22 -0700

    6/11

--- screenshot-to-jpg.sh
+++ screenshot-to-jpg.sh
@@ -3,6 +3,6 @@ set -e
 for file in Screenshot*.png; do
   [ -e "$file" ] || continue
   output="${file%.png}.jpg"
-  magick "$file" -quality 80 "$output"
+  magick "$file" -quality 70 "$output"
   echo "new image: $output"
 done
